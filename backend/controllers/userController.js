89b22c550d07be342caac6164a4aad02bb235e9d
githubankush <ankushkumayu04@gmail.com>
2025-07-13 12:54:44 +0530

    cookie settings (secure,sameSite)updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,8 +63,8 @@ const login = async (req, res) => {
         // ✅ Set cookie
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'None',
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         });
 
@@ -104,8 +104,8 @@ const profile = async (req, res) => {
 const logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None"
   });
   res.status(200).json({ message: "User logged out successfully" });
 };
